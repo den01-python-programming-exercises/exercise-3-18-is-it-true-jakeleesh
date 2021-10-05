51d03557d343cc518d732e258efca38e7b5799a0
Jake Lee <jake.lee@fdmgroup.com>
2021-10-05 11:01:01 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    user_input = input()
+    if (user_input == "True"):
+        print("You got it right!")
+    else:
+        print("Try again!")
 
 if __name__ == '__main__':
     main()
